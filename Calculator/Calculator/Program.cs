151b7117480284a8e866c7b56c46c4f83e4826d1
Tanner Percival <percivaltanner@gmail.com>
2019-11-15 15:42:05 -0800

    optimized the code and made it more DRY

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -9,36 +9,25 @@ namespace Calculator
             //declares variables to work with 
             int param1;
             int param2;
-            string operation;
+            
             string[] restart = new string[] { "" };
+
+            Console.Clear();
             //welcome
             Console.WriteLine("welcome to my calculator!");
+
             //request user input and store int value into param1
             Console.WriteLine("Please select your first number: ");
             param1 = Convert.ToInt32(Console.ReadLine());
+
             //request second number and store to param 2
             Console.WriteLine("Please select your second number");
             param2 = Convert.ToInt32(Console.ReadLine());
-            //request and store operation type
-            Console.Write("now, select your operation (+ , - , x , /)");
-            operation = Console.ReadLine();
-            //if statement selecting which method to use based on operation string value
-            if (operation == "x")
-            {
-                Console.WriteLine($"your anwser is : {Mult(param1, param2)}");
-            }
-            if (operation == "/")
-            {
-                Console.WriteLine($"your answer is: {Divide(param1, param2)}");
-            }
-            if (operation == "+")
-            {
-                Console.WriteLine($"your answer is: {Add(param1, param2)}");
-            }
-            if (operation == "-")
-            {
-                Console.WriteLine($"your answer is: {Subtract(param1, param2)}");
-            }
+
+
+
+            Console.WriteLine(SelectOperator(param1, param2));
+           
             Console.ReadLine();
             Console.WriteLine("to do another calculation please type : \"Restart\" ");
             restart[0] = Console.ReadLine();
@@ -46,6 +35,42 @@ namespace Calculator
             {
                 Main(restart);
             }
+        }
+        public static string SelectOperator( int param1, int param2)
+        {
+
+            string result;
+            string operation;
+            //request and store operation type
+            Console.Write("now, select your operation (+ , - , x , /)");
+            operation = Console.ReadLine();
+            //if statement selecting which method to use based on operation string value
+            if (operation == "x")
+            {
+                result = $"your anwser is : {Mult(param1, param2)}";
+                return result;
+            }
+            if (operation == "/")
+            {
+                result = $"your answer is: {Divide(param1, param2)}";
+                return result;
+            }
+            if (operation == "+")
+            {
+                result = $"your answer is: {Add(param1, param2)}";
+                return result;
+            }
+            if (operation == "-")
+            {
+                result = $"your answer is: {Subtract(param1, param2)}";
+                return result;
+            }
+            else
+            {
+                Console.WriteLine("You entered an invalid operator, please try again");
+                SelectOperator(param1, param2);
+            }
+            return "something went wrong oops!";
         }
         public static int Mult(int param1, int param2)
         {
